4bdd46a84cfe5e693583e6f193f34588c1d416ed
catul0 <valesigaudo1@gmail.com>
2022-11-03 11:06:28 -0300

    mock de tareas

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] =[
+    {
+        id: 1,
+        text: "terminar nuestro primer modulo de Angular",
+        day: 'noviembre 5',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "sacar la ropa del lavaropas",
+        day: 'noviembre 3',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "terminar con el curso",
+        day: 'enero 2',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "cocinar",
+        day: 'noviembre 3 a las 11:00 am',
+        reminder: false
+    }
+]
