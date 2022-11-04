a4b38f9be4c6bc0e8ea3fbbeed5a30a9d66e907d
catul0 <valesigaudo1@gmail.com>
2022-11-04 19:36:40 -0300

    pasamos las tareas al servicio

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Task } from '../../Task';
-import { TASKS } from '../../mock-task';
+import { TaskService } from '../../service/task.service';
+import {Task} from '../../Task';
 
 @Component({
   selector: 'app-tasks',
@@ -8,11 +8,15 @@ import { TASKS } from '../../mock-task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[]= TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { 
+    
+  }
 
   ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
   }
+  
 
 }
