e97e0d9acb5b414092465e8a582bc232bdad0632
catul0 <valesigaudo1@gmail.com>
2022-11-07 08:35:49 -0300

    servdiro casi terminado

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHandler} from '@angular/common/http'
+import { Observable, of} from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-task';
 
@@ -6,9 +8,12 @@ import { TASKS } from '../mock-task';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
-  getTasks(): Task[]{
-    return TASKS
+  constructor(
+    private http:HttpClient
+  ) { }
+  getTasks(): Observable <Task[]>{
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
