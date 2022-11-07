b8f730bc2ad914c44df0ce449122e1e85ae767b6
catul0 <valesigaudo1@gmail.com>
2022-11-07 08:52:11 -0300

    servidor de task terminado

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -13,7 +13,7 @@ export class TaskService {
   constructor(
     private http:HttpClient
   ) { }
-  getTasks(): Observable <Task[]>{
+  getTasks(): Observable<Task[]>{
     return this.http.get<Task[]>(this.apiUrl)
   }
 }
